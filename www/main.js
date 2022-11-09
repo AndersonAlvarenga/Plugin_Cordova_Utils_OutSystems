8e2baaf9b44478d96723086ea9ab07822181655c
AndersonAlvarenga <andersonalvarenga15@gmail.com>
2022-11-09 15:04:04 -0300

    trocado lib

--- www/main.js
+++ www/main.js
@@ -1,8 +1,8 @@
 var exec = require('cordova/exec');
 
 //Pagamento
-exports.getTitulo = function (success, error) {
-    exec(success, error, 'MainActivity', 'getTitulo');
+exports.getVersion = function (success, error) {
+    exec(success, error, 'MainActivity', 'getVersion');
 };
 exports.pagamento = function (params, success, error) {
     exec(success, error, 'MainActivity', 'pagamento', [params]);
@@ -20,51 +20,3 @@ exports.GetDadosTransacao = function (success, error) {
     exec(success, error, 'MainActivity', 'GetDadosTransacao');
 };
 
-
-//Metodos Impressora
-exports.checarImpressora = function (success, error) {
-    exec(success, error, 'MainActivity', 'checarImpressora');
-};
-exports.imprimir = function (params, success, error) {
-    exec(success, error, 'MainActivity', 'imprimir', [params]);
-};
-exports.impressoraOutput = function (params, success, error) {
-    exec(success, error, 'MainActivity', 'impressoraOutput', [params]);
-};
-
-//Metodo Beep
-exports.beep = function (success, error) {
-    exec(success, error, 'MainActivity', 'beep');
-};
-
-//Metodos Led
-exports.ledOn = function (success, error) {
-    exec(success, error, 'MainActivity', 'ledOn');
-};
-exports.ledOff = function (success, error) {
-    exec(success, error, 'MainActivity', 'ledOff');
-};
-exports.ledRedOn = function (success, error) {
-    exec(success, error, 'MainActivity', 'ledRedOn');
-};
-exports.ledBlueOn = function (success, error) {
-    exec(success, error, 'MainActivity', 'ledBlueOn');
-};
-exports.ledGreenOn = function (success, error) {
-    exec(success, error, 'MainActivity', 'ledGreenOn');
-};
-exports.ledOrangeOn = function (success, error) {
-    exec(success, error, 'MainActivity', 'ledOrangeOn');
-};
-exports.ledRedOff = function (success, error) {
-    exec(success, error, 'MainActivity', 'ledRedOff');
-};
-exports.ledBlueOff = function (success, error) {
-    exec(success, error, 'MainActivity', 'ledBlueOff');
-};
-exports.ledGreenOff = function (success, error) {
-    exec(success, error, 'MainActivity', 'ledGreenOff');
-};
-exports.ledOrangeOff = function (success, error) {
-    exec(success, error, 'MainActivity', 'ledOrangeOff');
-};
